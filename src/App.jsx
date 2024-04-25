ff98f7541dceec34bd0d9f26583155e51bfcc645
iqramahamed <iqra.mahamed64@gmail.com>
2024-04-25 13:58:25 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Header from './layout/Header'
+import Footer from './layout/Fotter';
+
+function App() { // main componenet
+  
+
+  return (
+    <div>
+      <Header/>
+      Home
+      <Footer/>
+      </div>
+  
+    
+  );
+}
+
+export default App
---
+++ src/layout/Header.jsx
@@ -0,0 +1,19 @@
+/**
+ * 
+ * components
+ * 
+ * 1.componenets must always start with a capital letter
+ * 2.Must always return a single parent element
+ * 3.Must be functions(normal functions or arrow functions)
+ * 
+ */
+
+ const Header = () => {
+  return (
+    <div>
+        <header>Header</header>
+        <h1>React Starter</h1>
+        </div>
+  )
+}
+export default Header;
